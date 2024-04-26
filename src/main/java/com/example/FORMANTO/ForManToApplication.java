617c4dc1c93df06f24ac2732136e88ac3795ee7d
JEHY1 <113657783+JEHY1@users.noreply.github.com>
2024-04-26 17:42:04 +0900

    start

---
+++ src/main/java/com/example/FORMANTO/ForManToApplication.java
@@ -0,0 +1,13 @@
+package com.example.forManTo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForManToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForManToApplication.class, args);
+	}
+
+}
